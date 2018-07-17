5c5084c93a05f46035e30897adce880ef46896eb
Derek Hanciles <drhanciles@icloud.com>
2018-07-17 14:06:00 -0600

    Implementation of the user being able to add an idea has been completed

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-// Variables 
-var inputTitle = $('.js-input-title'); 
-var inputBody = $('.js-input-body'); 
-var submitBtn = $('.js-save-button'); 
+// Global Variables 
+var inputTitle = document.querySelector('.js-input-title'); 
+var inputBody = document.querySelector('.js-input-body'); 
+var submitBtn = document.querySelector('.js-save-button'); 
 
 // Event Listeners 
-submitBtn.on('click', createIdea); 
+submitBtn.addEventListener('click', addIdea); 
 
 // Functions 
-function createIdea() {
-  e.preventDefault(); 
-  var ideaTitle = inputTitle.val(); 
-  var ideaBody = inputBody.val(); 
-  var newIdea = $('<article>'.appendTo('section'); 
+function addIdea(event) {
+  event.preventDefault(); 
+  var newIdeaTitle = inputTitle.value.trim(); 
+  var newIdeaBody = inputBody.value.trim(); 
+  var newIdea = document.createElement('article');
+  createIdea(newideaTitle, newIdeaBody, newIdea);
+  main.appendChild(newIdea); 
+  clearInputs(); 
+};
 
-}
+function createIdea(newideaTitle, newIdeaBody, newIdea) {
+  newIdea.innerHTML = `<h3>${newideaTitle}</h3>
+        <img class="delete-button"src="images/delete.svg">
+        <p>${newIdeaBody}</p>
+        <img class="vote-buttons"src="images/upvote.svg">
+        <img class="vote-buttons"src="images/downvote.svg">
+        <h5 class="quality">quality: swill</h5>`
+}; 
+
+function clearInputs() {
+  inputTitle.value = '';
+  inputBody.value = ''; 
+}; 
