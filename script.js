d2defbe79431aa015ecc198242de1da728ff37b9
Derek Hanciles <drhanciles@icloud.com>
2018-07-17 16:27:32 -0600

    Update to variables that are undefined

--- script.js
+++ script.js
@@ -2,25 +2,27 @@
 var inputTitle = document.querySelector('.js-input-title'); 
 var inputBody = document.querySelector('.js-input-body'); 
 var submitBtn = document.querySelector('.js-save-button'); 
+var section = document.querySelector('.js-section'); 
 
 // Event Listeners 
-submitBtn.addEventListener('click', addIdea); 
+submitBtn.addEventListener('click', addIdea);
+console.log(submitBtn);  
 
 // Functions 
 function addIdea(event) {
   event.preventDefault(); 
-  var newIdeaTitle = inputTitle.value.trim(); 
-  var newIdeaBody = inputBody.value.trim(); 
+  var newInputTitle = inputTitle.value.trim(); 
+  var newInputBody = inputBody.value.trim(); 
   var newIdea = document.createElement('article');
-  createIdea(newideaTitle, newIdeaBody, newIdea);
-  main.appendChild(newIdea); 
+  createIdea(newInputTitle, newInputBody, newIdea);
+  section.appendChild(newIdea); 
   clearInputs(); 
 };
 
-function createIdea(newideaTitle, newIdeaBody, newIdea) {
-  newIdea.innerHTML = `<h3>${newideaTitle}</h3>
+function createIdea(newInputTitle, newInputBody, newIdea) {
+  newIdea.innerHTML = `<h3>${newInputTitle}</h3>
         <img class="delete-button"src="images/delete.svg">
-        <p>${newIdeaBody}</p>
+        <p>${newInputBody}</p>
         <img class="vote-buttons"src="images/upvote.svg">
         <img class="vote-buttons"src="images/downvote.svg">
         <h5 class="quality">quality: swill</h5>`
